fb3aae387c2709c701cb4dc72431e8b5fa8d104f
mard2499 <mige34.99@gmail.com>
2023-01-28 00:32:24 -0500

    prueba de navbar

--- ecomard/src/App.js
+++ ecomard/src/App.js
@@ -26,43 +26,44 @@ import {
 } from 'reactstrap';
 
 
-function App(args) {
+function App() {
   
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
 
 
   return (
-    <div>
-      <Navbar {...args}>
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="me-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">
-                GitHub
-              </NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>Option 1</DropdownItem>
-                <DropdownItem>Option 2</DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>Reset</DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-          <NavbarText>Simple Text</NavbarText>
-        </Collapse>
-      </Navbar>
+    <div className='app'>
+      <Router>
+        <Navbar color='dark' dark expand="md">
+          <NavbarBrand href="/">Ecomard</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="me-auto" navbar>
+
+              <NavItem>
+                <Link to="/">
+                  <NavLink>Temas</NavLink>
+                </Link>
+              </NavItem>
+
+              <NavItem>
+                <Link to="/">
+                  <NavLink>Crear</NavLink>
+                </Link>
+              </NavItem>
+
+            </Nav>
+
+          </Collapse>
+        </Navbar>
+      </Router>
+
+
+
+
     </div>
+
   );
 }
 
